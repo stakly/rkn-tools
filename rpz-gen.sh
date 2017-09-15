30656ab64d7207a4ba9f9c5047aa6236b08dacfd
root <root@www.svtk.ru>
2017-09-15 11:03:37 +0500

    script for gerenating bind RPZ zone based on https urls from rkn dump.xml

---
+++ rpz-gen.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+[ -z "$2" ] && echo "$0 <dump.xml> <rpz-dns-zone>" && exit 1
+xmlstarlet sel -R -t -m '//content' -s 'D:T:-' 'url' -c '.' "$1" | xmlstarlet sel \
+	-t \
+	-o '$TTL 1H	' \
+	-n \
+	-o '@				SOA LOCALHOST. root.localhost. (1 1h 15m 30d 2h)' \
+	-n \
+	-o '				NS  LOCALHOST.' \
+	-n \
+	-m 'set:distinct(//content/domain)' \
+	-s 'A:T:-' '.' \
+	--if '../@blockType="domain" or (not(../@blockType) and not(starts-with(../url,"http:"))) and string(number(translate(.,".","")))="NaN"' \
+	--if 'substring(.,string-length(.),1)="."' \
+		--var 'DOMAIN=substring(.,1,string-length(.)-1)' \
+		--if 'not(string(//content[domain=$DOMAIN and not(starts-with(url,"http:"))]))' \
+			-v '$DOMAIN' \
+			-o '			CNAME	lawfilter.magnitka.ru.' \
+			-n \
+			-o '*.' \
+			-v '$DOMAIN' \
+			-o '			CNAME	lawfilter.magnitka.ru.' \
+			-n \
+		--else --break \
+	--else \
+		--var 'DOMAIN=.' \
+		-v '$DOMAIN' \
+		-o '			CNAME	lawfilter.magnitka.ru.' \
+		-n \
+		-o '*.' \
+		-v '$DOMAIN' \
+		-o '			CNAME	lawfilter.magnitka.ru.' \
+		-n \
+	-t \
+	-m 'set:distinct(//content/domain)' \
+	-s 'A:T:-' '.' \
+	--if '../@blockType="domain-mask"' \
+	-v '.' \
+	-o '                    CNAME   lawfilter.magnitka.ru.' \
+	-n > "$2"
